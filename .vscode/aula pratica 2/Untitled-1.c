4348b7aebe360a600ffffc35d371381502524a30
Rafael H Bortoli <rafael.pafal927@gmail.com>
2025-10-30 14:07:16 -0300

    fix: Removendo arquivos adicionados por engano

--- .vscode/aula pratica 2/Untitled-1.c
+++
@@ -1,8 +0,0 @@
-# include <stdio.h>
-
-int main() {
-     int numeros[4] = {10, 20, 30, 40};
-     numeros[2] = 10; // Alterando o valor do terceiro elemento do array
-        printf("%d\n", numeros[2]);
-        return 0;
-}
